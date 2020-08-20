62a61c783d93d4ef16743ecb18d1b9edef110b5c
Tyler Ouellette <ouell117@uwindsor.ca>
2020-08-20 07:59:49 -0400

    yolo push

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,17 @@
-// let's go!
+require('dotenv').config({path: 'variables.env'});
+const createServer = require('./createServer');
+const db = require('./db');
+
+const server = createServer();
+
+// TODO Use express to handle cookies and JWT
+// TODO Use express middleware to populate current user
+
+server.start({
+    cors: {
+        credentials: true,
+        origin: process.env.FRONTEND_URL,
+    },
+}, details => {
+    console.log(`Sever is now running on port http://localhost:${details.port}`)
+})
