52919c3b1fe009ce3f61872cca9b69723d5a6d2a
Tyler Ouellette <ouell117@uwindsor.ca>
2020-08-29 16:04:03 -0400

    upto photos

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,17 +1,20 @@
-require('dotenv').config({path: 'variables.env'});
+require('dotenv').config({ path: 'variables.env' });
 const createServer = require('./createServer');
 const db = require('./db');
 
 const server = createServer();
 
-// TODO Use express to handle cookies and JWT
-// TODO Use express middleware to populate current user
+// TODO Use express middlware to handle cookies (JWT)
+// TODO Use express middlware to populate current user
 
-server.start({
+server.start(
+  {
     cors: {
-        credentials: true,
-        origin: process.env.FRONTEND_URL,
+      credentials: true,
+      origin: process.env.FRONTEND_URL,
     },
-}, details => {
-    console.log(`Sever is now running on port http://localhost:${details.port}`)
-})
+  },
+  deets => {
+    console.log(`Server is now running on port http://localhost:${deets.port}`);
+  }
+);
